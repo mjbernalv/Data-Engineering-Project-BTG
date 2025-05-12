d552cae68440e8aa438f29907ab239006f0953b3
Maria Jose Bernal <71986858+mjbernalv@users.noreply.github.com>
2025-05-12 12:03:18 -0500

    Create variables.tf

---
+++ iac/variables.tf
@@ -0,0 +1,8 @@
+# variables.tf
+variable "aws_region" {
+  default = "us-east-2" # cambiar a region de aws real
+}
+
+variable "bucket_name" {
+  default = "your_bucket_name" # cambiar por nombre del bucket real
+}
