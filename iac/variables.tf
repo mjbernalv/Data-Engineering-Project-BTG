8f903ded6502c4b600486427d5c45a8b4ed05ccc
Maria Jose Bernal <71986858+mjbernalv@users.noreply.github.com>
2025-05-12 12:28:27 -0500

    Update variables.tf

--- iac/variables.tf
+++ iac/variables.tf
@@ -5,3 +5,8 @@ variable "aws_region" {
 variable "bucket_name" {
   default = "your_bucket_name" # cambiar por nombre del bucket real
 }
+
+variable "database_name" {
+  description = "El nombre de la base de datos en AWS Glue"
+  type        = string
+}
